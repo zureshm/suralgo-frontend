391db2e87363f144b34936dacb399ef8f48fe72e
zureshm <zuresh.m@gmail.com>
2026-05-05 20:15:07 +0530

    FUCK FUCK FUCK

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
